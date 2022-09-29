5276991e41bda3a0f359c090dad2687322972bfa
andhikaa4 <110803805+andhikaa4@users.noreply.github.com>
2022-09-30 03:05:49 +0700

    Day 10

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"html/template"
 	"log"
-
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgtype"
+
 	"github.com/lib/pq"
 )
 
@@ -39,7 +39,6 @@ func main() {
 type Project struct {
 	ID           int
 	Title        string
-	Content      string
 	StartDate    pgtype.Date
 	EndDate      pgtype.Date
 	Node         string
@@ -47,10 +46,13 @@ type Project struct {
 	Next         string
 	Typescript   string
 	Technologies []string
+	Content      string
 	Duration     int
+	Format_Date1 string
+	Format_Date2 string
 }
 
-var dataProject = []Project{}
+// var dataProject = []Project{}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -61,7 +63,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data, _ := connection.Conn.Query(context.Background(), "SELECT id, name, start_date, end_date,technologies, description FROM tb_project")
+	data, _ := connection.Conn.Query(context.Background(), "SELECT id, name, start_date, end_date,technologies, description  FROM tb_project")
 
 	var result []Project
 	for data.Next() {
@@ -73,6 +75,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		each.Node = each.Technologies[0]
+		each.React = each.Technologies[1]
+		each.Next = each.Technologies[2]
+		each.Typescript = each.Technologies[3]
+
+		date := "2 Jan 2006"
+		each.Format_Date1 = each.StartDate.Time.Format(date)
+		each.Format_Date2 = each.EndDate.Time.Format(date)
+
+		each.Duration = int(each.EndDate.Time.Month() - each.StartDate.Time.Month())
+
 		result = append(result, each)
 	}
 
@@ -80,8 +93,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 		"Project": result,
 	}
 
-	fmt.Println(result)
-
+	// fmt.Println(result)
+	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w, response)
 }
 
@@ -103,39 +116,21 @@ func postProject(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	// var title = r.PostForm.Get("projectName")
-	// var content = r.PostForm.Get("Description")
-	// var startDate = r.PostForm.Get("sd")
-	// var endDate = r.PostForm.Get("ed")
-	// var node = r.PostForm.Get("c1")
-	// var react = r.PostForm.Get("c2")
-	// var next = r.PostForm.Get("c3")
-	// var typescript = r.PostForm.Get("c4")
+	var title = r.PostForm.Get("projectName")
+	var startDate = r.PostForm.Get("sd")
+	var endDate = r.PostForm.Get("ed")
+	var node = r.PostForm.Get("c1")
+	var react = r.PostForm.Get("c2")
+	var next = r.PostForm.Get("c3")
+	var typescript = r.PostForm.Get("c4")
+	var content = r.PostForm.Get("Description")
 
-	// format := "2006-01-02"
-	// startDateD, _ := time.Parse(format, startDate)
-	// endDateD, _ := time.Parse(format, endDate)
-
-	// hour := endDateD.Sub(startDateD).Hours()
-	// days := hour / 24
-	// week := days / 7
-	// month := week / 4
-
-	// var getMonth, _ float64 = math.Modf(month)
-
-	// var postProject = Project{
-	// 	Title:      title,
-	// 	Content:    content,
-	// 	StartDate:  startDate,
-	// 	EndDate:    endDate,
-	// 	Node:       node,
-	// 	React:      react,
-	// 	Next:       next,
-	// 	Typescript: typescript,
-	// 	Duration:   int(getMonth),
-	// }
-
-	// dataProject = append(dataProject, postProject)
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_project(name, start_date, end_date,technologies, description) VALUES ($1, $2, $3, $4, $5)", title, startDate, endDate, pq.Array([]string{node, react, next, typescript}), content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
 
 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
@@ -182,21 +177,21 @@ func projectDetail(w http.ResponseWriter, r *http.Request) {
 
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-	for i, data := range dataProject {
-		if id == i {
-			ProjectDetail = Project{
-				Title:      data.Title,
-				Content:    data.Content,
-				StartDate:  data.StartDate,
-				EndDate:    data.EndDate,
-				Node:       data.Node,
-				React:      data.React,
-				Next:       data.Next,
-				Typescript: data.Typescript,
-				Duration:   data.Duration,
-			}
-		}
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, name,  start_date, end_date, technologies,description FROM tb_project WHERE id=$1", id).Scan(
+		&ProjectDetail.ID, &ProjectDetail.Title, &ProjectDetail.StartDate, &ProjectDetail.EndDate, &ProjectDetail.Technologies, &ProjectDetail.Content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
 	}
+
+	ProjectDetail.Node = ProjectDetail.Technologies[0]
+	ProjectDetail.React = ProjectDetail.Technologies[1]
+	ProjectDetail.Next = ProjectDetail.Technologies[2]
+	ProjectDetail.Typescript = ProjectDetail.Technologies[3]
+
+	date := "2 Jan 2006"
+	ProjectDetail.Format_Date1 = ProjectDetail.StartDate.Time.Format(date)
+	ProjectDetail.Format_Date2 = ProjectDetail.EndDate.Time.Format(date)
 
 	data := map[string]interface{}{
 		"Project": ProjectDetail,
@@ -208,7 +203,11 @@ func projectDetail(w http.ResponseWriter, r *http.Request) {
 func deleteProject(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-	dataProject = append(dataProject[:id], dataProject[id+1:]...)
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_project WHERE id=$1", id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+	}
 
 	http.Redirect(w, r, "/", http.StatusFound)
 }
@@ -226,20 +225,11 @@ func getProject(w http.ResponseWriter, r *http.Request) {
 
 	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-	for i, data := range dataProject {
-		if id == i {
-			EditProject = Project{
-				Title:      data.Title,
-				Content:    data.Content,
-				StartDate:  data.StartDate,
-				EndDate:    data.EndDate,
-				Node:       data.Node,
-				React:      data.React,
-				Next:       data.Next,
-				Typescript: data.Typescript,
-				Duration:   data.Duration,
-			}
-		}
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, name,  start_date, end_date, technologies,description FROM tb_project WHERE id=$1", id).Scan(
+		&EditProject.ID, &EditProject.Title, &EditProject.StartDate, &EditProject.EndDate, &EditProject.Technologies, &EditProject.Content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
 	}
 
 	data := map[string]interface{}{
@@ -255,16 +245,25 @@ func editProject(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	// id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-	// dataProject[id].Title = r.PostForm.Get("projectName")
-	// dataProject[id].Content = r.PostForm.Get("Description")
-	// dataProject[id].StartDate = r.PostForm.Get("sd")
-	// dataProject[id].EndDate = r.PostForm.Get("ed")
-	// dataProject[id].Node = r.PostForm.Get("c1")
-	// dataProject[id].React = r.PostForm.Get("c2")
-	// dataProject[id].Next = r.PostForm.Get("c3")
-	// dataProject[id].Typescript = r.PostForm.Get("c4")
+	var title = r.PostForm.Get("projectName")
+	var startDate = r.PostForm.Get("sd")
+	var endDate = r.PostForm.Get("ed")
+	var node = r.PostForm.Get("c1")
+	var react = r.PostForm.Get("c2")
+	var next = r.PostForm.Get("c3")
+	var typescript = r.PostForm.Get("c4")
+	var content = r.PostForm.Get("Description")
+
+	Update := `UPDATE tb_project SET name=$2, start_date=$3, end_date=$4, technologies=$5, description=$6 WHERE id=$1`
+
+	_, err = connection.Conn.Exec(context.Background(), Update, id, title, startDate, endDate, pq.Array([]string{node, react, next, typescript}), content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
 
 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
